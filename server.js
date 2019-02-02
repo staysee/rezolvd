e53d0d85787c36fc70324d326b4848877041f534
Stacey Sugiono <sugiono.stacey@gmail.com>
2019-02-01 18:26:03 -0800

    setting up server and added config

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const {DATABASE_URL, PORT} = require('./config');
+
+const jsonParser = bodyParser.json();
+const app = express();
+
+app.use(morgan('common'));
+app.use(express.static('client'));
+
+//CORS
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+    if (req.method === 'OPTIONS') {
+      return res.send(204);
+    }
+    next();
+  });
+
+//all endpoints
+app.get('*', (req, res) => res.send('ok'));
+
+// OPEN/CLOSE SERVER
+let server;
+
+function runServer(databaseUrl, port=PORT) {
+	return new Promise((resolve, reject) => {
+		mongoose.connect(databaseUrl, err => {
+			if (err) {
+				return reject(err);
+			}
+
+			server = app.listen(port, () => {
+				console.log(`Your app is listening on port ${port}`);
+				resolve();
+			})
+			.on('error', err => {
+				mongoose.disconnect();
+				reject(err);
+			});
+		});
+	});
+}
+
+function closeServer() {
+	return mongoose.disconnect().then(() => {
+		return new Promise((resolve, reject) => {
+			console.log('Closing server');
+			server.close(err => {
+				if (err) {
+					return reject(err);
+				}
+				resolve();
+			});
+		});
+	});
+}
+
+if (require.main === module) {
+	runServer(DATABASE_URL).catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer }; 
