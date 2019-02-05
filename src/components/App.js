253a1770ad35d0481ab307c85537e2e09f684eab
Stacey Sugiono <sugiono.stacey@gmail.com>
2019-02-04 18:13:22 -0800

    working on connecting to api

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import './App.css';
+
+class App extends React.Component {
+
+	
+	render() {
+    	return (
+			<ul>
+			</ul>
+    	);
+  	}
+}
+
+export default App;
