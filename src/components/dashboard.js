45073120bda744dfd49dd1ed90aae60ee50bce37
Stacey Sugiono <sugiono.stacey@gmail.com>
2019-03-31 18:18:04 -0700

    action steps and screen

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -2,13 +2,24 @@ import React from 'react';
 
 import Navigation from './navigation';
 import Venues from './venues';
+import '../styles/dashboard.css';
 
 export default class Dashboard extends React.Component {
     render() {
         return (
             <main>
                 <Navigation />
-                <Venues />
+
+                <div className="container">
+                    <section className="actions">
+                        <div className="action-step">Find My Favorites</div>
+                        <div className="action-step">Pick My Clique</div>
+                        <div className="action-step">Choose My Hangout</div>
+                    </section>
+                    <section className="screen">
+                        <Venues />
+                    </section>
+                </div>
             </main> 
         )
     }
